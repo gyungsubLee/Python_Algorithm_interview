4003bf6b1de854103a38462588a0f77bccf5e377
gyungsubLee <dltjq2323@gamil.com>
2022-07-07 15:24:45 +0900

    Fixed #7 issue7/chapter3Generic, any, 예시 추가 및 내용 수정

--- 백준/ex.py
+++ 백준/ex.py
@@ -1,17 +1,4 @@
-# 그리드 알고리즘
-# 아이디어: `각 행마다 가장 작은 수를 찾은 뒤에 그 수 중에서 가장 큰수 찾기`
 
-# 퓰아1) - 내 풀이
-n, m = map(int, input().split())
-min_list = []
-
-for _ in range(n):
-    data = list(map(int, input().split()))
-    min_list.append(min(data))
-
-result = max(min_list)
-
-print(result)
 
 
 
