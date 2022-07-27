02b7288e676763b7dd176d279d04d818b5fa01c7
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-07-27 14:52:45 +0900

    Time: 137 ms (67.98%), Space: 17.1 MB (96.43%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,7 +1,19 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = collections.defaultdict(list)
         
-        for word in strs:
-            anagrams[''.join(sorted(word))].append(word)
-        return list(anagrams.values())
+        if strs == [""]:
+            return [strs]
+    
+        ans = {}
+        
+        for i in strs:
+            sorted_i = "".join(sorted(i))
+            if sorted_i in ans:
+                ans[sorted_i].append(i)
+            else:
+                ans[sorted_i] = [i]
+                
+        res = []
+        for i in ans.values():
+            res.append(i)
+        return res
