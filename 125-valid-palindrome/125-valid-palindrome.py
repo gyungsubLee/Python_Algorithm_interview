3fb6956dfa9759b98e1ce1b60d867b1dac51f53f
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-07-27 10:08:43 +0900

    Time: 303 ms (5.03%), Space: 19.3 MB (8.76%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs=[]
+        for char in s:
+            if char.isalnum(): # isalnum(): 영문자, 숫자 여부 판단(영:True, 숫자:False)
+                strs.append(char.lower()) # lower(): 소문자 변환
+        
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False
+        
+        
+        return True
+                
