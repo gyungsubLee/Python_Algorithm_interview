bf6593bf2235c017041e6048352ec63490a409ce
gyungsubLee <dltjq2323@gamil.com>
2022-07-27 13:05:27 +0900

    Add 로그파일재정렬(n.937)/Notes/풀이1 정리

--- Leetcode/937-reorder-data-in-log-files/풀이1-람다.py
+++ Leetcode/937-reorder-data-in-log-files/풀이1-람다.py
@@ -1,10 +1,12 @@
 class Solution:
-    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+    def reorderLogFiles(self, logs):
         letters, digits = [], []
         for log in logs:
+            print(log.split()[1])
             if log.split()[1].isdigit():
                 digits.append(log)
             else:
                 letters.append(log)
         letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
         return letters + digits
+
