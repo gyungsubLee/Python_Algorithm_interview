b81a858de0faa9282820e558407b8296947f316b
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-07-27 14:02:22 +0900

    Time: 73 ms (18.72%), Space: 13.8 MB (83.08%) - LeetHub

--- 819-most-common-word/819-most-common-word.py
+++ 819-most-common-word/819-most-common-word.py
@@ -2,7 +2,6 @@ import collections
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         words = [word for word in re.sub('[^\w]', " ", paragraph).lower().split() if word not in banned]
-        counts = collections.defaultdict(int)
-        for word in words:
-            counts[word] += 1
-        return max(counts, key=counts.get)
+        counts = collections.Counter(words)
+
+        return counts.most_common(1)[0][0]
