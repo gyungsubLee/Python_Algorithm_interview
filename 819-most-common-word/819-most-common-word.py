f228dbbd46cfc83ddc67848d38e0f4ae54db2bfa
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-07-28 09:58:02 +0900

    Time: 46 ms (74.05%), Space: 14 MB (37.14%) - LeetHub

--- 819-most-common-word/819-most-common-word.py
+++ 819-most-common-word/819-most-common-word.py
@@ -1,7 +1,7 @@
 import collections
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        words =[word for word in re.sub('[^\w]',' ', paragraph.lower()).split() if word not in banned]
+        words =[word for word in re.sub('[^\w]',' ', paragraph).lower().split() if word not in banned]
         Fre = collections.defaultdict(int)
         for word in words:
             Fre[word] += 1
