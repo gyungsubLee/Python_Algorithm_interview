00523fef423a7c042493322b488ebec15f55794b
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-07-29 10:47:11 +0900

    Time: 90 ms (69.59%), Space: 14.9 MB (77.26%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,9 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_map = {}
-        for i, num in enumerate(nums):
-            if target -num in nums_map:
-                return [i, nums_map[target-num]]
-            nums_map[num] = i
+        nums_copy = sorted(nums)
+        left, right = 0, len(nums)-1
+        while not left == right:
+            if nums_copy[left] + nums_copy[right] < target:
+                left += 1
+
+            elif nums_copy[left] + nums_copy[right] > target:
+                right -= 1
+
+            elif nums_copy[left] != nums_copy[right]:
+                return [nums.index(nums_copy[left]), nums.index(nums_copy[right])]
+            
+            else:
+                return [nums.index(nums_copy[left]), nums.index(nums_copy[right] ,nums.index(nums_copy[left])+1, len(nums)) ]
                 
             
