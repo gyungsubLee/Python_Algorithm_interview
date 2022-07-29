fa2ec52e4d2c99aae17e19b471fd4e1c541d79b2
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-07-30 00:54:53 +0900

    Time: 468 ms (33.09%), Space: 17 MB (17.97%) - LeetHub

--- 561-array-partition/561-array-partition.py
+++ 561-array-partition/561-array-partition.py
@@ -1,13 +1,10 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        pair = []
         sum = 0
         nums.sort()
         
-        for n in nums:
-            pair.append(n)
-            if len(pair) == 2:
-                sum += pair[0]
-                pair = []
+        for i, n in enumerate(nums):
+            if i % 2 == 0:
+                sum += n
             
         return sum
