27c8a86862d79c9ef39e256ec7e99760ed6d0080
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-07-29 10:29:59 +0900

    Time: 60 ms (97.18%), Space: 15.1 MB (50.46%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,7 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i, n in enumerate(nums):
-            complate = target - n
-            
-            if complate in nums[i+1:]:
-                return [i, nums[i+1:].index(complate)+ i+1]
+        nums_map = {}
+        for i, num in enumerate(nums):
+            nums_map[num] = i
+                
+        for i, num in enumerate(nums):
+            if target -num in nums_map and i != nums_map[target -num]:
+                return [i, nums_map[target-num]]
