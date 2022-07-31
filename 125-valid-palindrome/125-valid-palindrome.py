89851cad46f6f15fe4db0d3f4b9b71121a76afcd
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-07-31 13:57:49 +0900

    Time: 71 ms (58.66%), Space: 15.5 MB (17.51%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,7 +1,7 @@
 from collections import deque
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        a = re.sub('[^a-z0-9]', '', s.lower())
+        a = re.sub('[^a-zA-Z0-9]', '', s).lower()
         return a == a[::-1]
         
             
