a3993a514080d47c892e00f4d58df3d4d4bcd8d2
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-08-02 13:58:54 +0900

    Time: 1092 ms (68.17%), Space: 18.1 MB (41.07%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -3,13 +3,13 @@ class Solution:
         res = []
         nums.sort()
         for i in range(len(nums)-2):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
+            if i>0 and nums[i] == nums[i-1]:
+                continue     
             l, r = i+1, len(nums)-1
             while l < r:
                 s = nums[i] + nums[l] + nums[r]
                 if s < 0:
-                    l +=1 
+                    l += 1
                 elif s > 0:
                     r -= 1
                 else:
@@ -19,6 +19,7 @@ class Solution:
                     while l < r and nums[r] == nums[r-1]:
                         r -= 1
                     l += 1; r -= 1
+
         return res
         
                     
