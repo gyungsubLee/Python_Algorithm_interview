a6e5f310aecda79ab627c106ca951e84909533b1
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-08-12 15:09:51 +0900

    Time: 55 ms (32.08%), Space: 14 MB (11.16%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -1,8 +1,3 @@
 class Solution:
     def numJewelsInStones(self, J: str, S: str) -> int:
-        counter = 0
-        jewels = set(J) # search in a set is instant - O(1)
-        for stone in S:
-            if stone in jewels:
-                counter += 1
-        return counter
+    	return sum(i in J for i in S)
