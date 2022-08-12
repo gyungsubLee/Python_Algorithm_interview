21b2a0ee7929d8d41170f9c94d3708d9c92ada44
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-08-12 14:46:36 +0900

    Time: 55 ms (32.08%), Space: 13.9 MB (58.50%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -1,13 +1,11 @@
+from collections import defaultdict
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        freqs = {}
+        freqs = defaultdict(int)
         count = 0
         
         for char in stones:
-            if char not in freqs:
-                freqs[char] = 1
-            else:
-                freqs[char] += 1
+            freqs[char] += 1
                 
         # 보석(J)의 빈도 수 합산
         for char in jewels:
