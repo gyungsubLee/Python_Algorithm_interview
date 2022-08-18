3e8eb58716ddf32465eeed262a19f6dd62d56423
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-08-18 23:24:12 +0900

    Time: 40 ms (73.28%), Space: 13.9 MB (79.63%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,5 +1,9 @@
 class Solution(object):
     def letterCombinations(self, digits:str)->List[str]:
+        # 예외처리
+        if not digits:
+            return []
+        
         # dfs - 재귀
         def dfs(index:int, path:str):
             if len(path) == len(digits):
@@ -9,10 +13,6 @@ class Solution(object):
             for word in words:
                 dfs(index+1, path+word)
                 
-        # 예외처리
-        if not digits:
-            return []
-        
         dic = { "2": "abc", "3": "def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
         
         res = []
