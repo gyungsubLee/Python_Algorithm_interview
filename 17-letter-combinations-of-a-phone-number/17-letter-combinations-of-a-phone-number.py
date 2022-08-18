f459d3438e7cd56f1ef710011a523352e7587e60
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-08-18 17:40:32 +0900

    Time: 32 ms (92.56%), Space: 13.9 MB (32.00%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,30 +1,22 @@
-class Solution:
+class Solution(object):
     def letterCombinations(self, digits):
-        if not digits:
-            return []
-        d = {
-            '2': 'abc',
-            '3': 'def',
-            '4': 'ghi',
-            '5': 'jkl',
-            '6': 'mno',
-            '7': 'pqrs',
-            '8': 'tuv',
-            '9': 'wxyz'
-        }
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        dic = { "2": "abc", "3": "def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
         
         res=[]
-        stack=[(0, "")] # index: int
-        while stack:
-            i, combo = stack.pop()
-            if i == len(digits):
-                res.append(combo)
-            else:
-                nextDigit =digits[i]
-                children  = d[nextDigit]
-                for child in children :
-                    stack.append((i+1, combo+child))
-        return res
-        
-        
+        if len(digits) ==0:
+            return res
             
+        self.dfs(digits, 0, dic, '', res)
+        return res
+    
+    def dfs(self, nums, index, dic, path, res):
+        if index >=len(nums):
+            res.append(path)
+            return
+        string1 =dic[nums[index]]
+        for i in string1:
+            self.dfs(nums, index+1, dic, path + i, res)
