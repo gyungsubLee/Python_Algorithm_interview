bb6d9b097c8e429de962fa0ac9a94e27050af6ae
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-08-19 00:06:56 +0900

    Time: 60 ms (57.42%), Space: 14 MB (84.57%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,11 +1,13 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        self.dfs(nums, [], res)
+        stack = [(nums, [])]
+        while stack:
+            nums, path = stack.pop()
+            if not nums:
+                res.append(path)
+            for i in range(len(nums)):
+                newNums = nums[:i] + nums[i+1:]
+                stack.append((newNums, path+[nums[i]]))
         return res
-    
-    def dfs(self, _nums, path, res):
-        if not _nums:
-            res.append(path)
-        for i in range(len(_nums)):
-            self.dfs(_nums[i+1:]+_nums[:i], path+[_nums[i]], res) # [1] + [2] = [1, 2]
+                
