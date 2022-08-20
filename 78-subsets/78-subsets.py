d861fa1857e474172f070fb240d0072e65242969
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-08-20 12:12:41 +0900

    Time: 52 ms (53.89%), Space: 14.1 MB (35.80%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,11 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res=[]
-        stack=[(0, [])]
-        while stack:
-            start, path = stack.pop()
+        def dfs(start, path):
             res.append(path)
             for i in range(start, len(nums)):
-                stack.append((i+1, path+[nums[i]]))
+                dfs(i+1, path+[nums[i]])
+
+        res=[]
+        dfs(0, [])
         return res
         
