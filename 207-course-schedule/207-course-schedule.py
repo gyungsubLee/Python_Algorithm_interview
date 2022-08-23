f0f91fe1c550caa8d5019e65f3a41c7c957680e8
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-08-23 23:32:41 +0900

    Time: 186 ms (29.18%), Space: 17.5 MB (11.49%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -3,7 +3,6 @@ from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
-        # 그래프 구성
         for x, y in prerequisites:
             graph[x].append(y)
         
@@ -11,11 +10,9 @@ class Solution:
         visited = set()
         
         def dfs(i):
-            # 순환구조 -> False
             if i in traced:
                 return False
             
-            # 이미 방문했던 노드
             if i in visited:
                 return True
             
@@ -24,14 +21,11 @@ class Solution:
                 if not dfs(y): 
                     return False
             
-            # 탐색 종료 후 삭제
             traced.remove(i)
-            # 탐색 종료 후 방문 노드 추가
             visited.add(i)
             
             return True
             
-        # 순환 구조 판별
         for x in list(graph):
             if not dfs(x):
                 return False
