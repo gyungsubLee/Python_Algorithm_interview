5ab11bd44b780ca5fba34bf89a65f7bbb8599a81
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-08-24 13:35:45 +0900

    Time: 358 ms (33.42%), Space: 15.1 MB (73.36%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -1,18 +1,17 @@
-import collections
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        graph = collections.defaultdict(dict)
-        for s, d, w in flights:
-            graph[s][d] = w
-        pq = [(0, src, k+1)]
-        vis = [0] * n
-        while pq:
-            w, x, k = heapq.heappop(pq)
-            if x == dst:
-                return w
-            if vis[x] >= k:
-                continue
-            vis[x] = k
-            for y, dw in graph[x].items():
-                heapq.heappush(pq, (w+dw, y, k-1))
-        return -1
+        prices = [float("inf")] * n
+        prices[src] = 0
+        
+        for i in range(k+1):
+            tmpPrices = prices.copy()
+            
+            for s, d, p in flights: # s=source, d=destination, p=price
+                if prices[s] == float("inf"):
+                    continue
+                if prices[s] + p < tmpPrices[d]:
+                    tmpPrices[d] = prices[s] + p
+            
+            prices = tmpPrices
+            
+        return -1 if prices[dst] == float("inf") else prices[dst]
