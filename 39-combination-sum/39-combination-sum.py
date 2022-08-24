c1c77856996e8d61458df23b69fb6592b8c8db3c
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-08-24 15:51:53 +0900

    Time: 92 ms (84.10%), Space: 13.9 MB (99.37%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,17 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        def dfs(index, target, path):
+        res = []
+        stack = [(0, target, [])]
+        while stack:
+            index, target, path = stack.pop()
             if target < 0:
-                return
+                continue
             if target == 0:
                 res.append(path)
-                return
-            
+                continue
             for i in range(index, len(candidates)):
-                dfs(i, target-candidates[i], path+[candidates[i]])
-                
-        res = []
-        dfs(0, target, [])
+                stack.append((i, target-candidates[i], path+[candidates[i]]))
+            
         return res
         
     
