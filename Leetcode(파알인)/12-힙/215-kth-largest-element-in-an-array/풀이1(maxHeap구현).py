ef98c69a37a28c651454c73c43bdb44c1936487d
gyungsubLee <gyungsub@DESKTOP-1JB0K0U.localdomain>
2022-09-17 14:55:10 +0900

    Fix #10 파이썬/힙/배열의K번째큰요소/풀이1(maxHeap구현)

--- Leetcode(파알인)/12-힙/215-kth-largest-element-in-an-array/풀이1(maxHeap구현).py
+++ Leetcode(파알인)/12-힙/215-kth-largest-element-in-an-array/풀이1(maxHeap구현).py
@@ -1,58 +1,53 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # insert
-        def insert_maxheap(n):
-            maxHeap.append(n)
-            i = len(maxHeap)-1
+        # maxHeap에 요소 삽입 및 정렬
+        def insert_maxHeap(n):
+            heap.append(n)
+            i = len(heap)-1
             parent = i//2
             while parent > 0:
-                if maxHeap[i] > maxHeap[parent]:
-                    maxHeap[parent], maxHeap[i] = maxHeap[i], maxHeap[parent]
+                if heap[i] > heap[parent]:
+                    heap[i], heap[parent] = heap[parent], heap[i]
                 i = parent
                 parent = i//2
-         
-        # extraction
-        def extraction_maxHeap(idx):
-            left = idx * 2
-            right = idx * 2 + 1
-            smallest = idx
-            
-            if left <= len(maxHeap) -1 and maxHeap[left] > maxHeap[smallest]:
-                smallest = left
-                
-            if right <= len(maxHeap) -1 and maxHeap[right] > maxHeap[smallest]:
-                smallest = right
-            
-            if smallest != idx:
-                maxHeap[idx], maxHeap[smallest] = maxHeap[smallest], maxHeap[idx]
-                extraction_maxHeap(smallest)
         
+        # 최댓값 추출 후 힙 재정렬
+        def rearrangement_maxHeap(idx):
+            left = idx*2
+            right = idx*2 + 1
+            largest = idx
+            
+            if left <= len(heap)-1 and heap[largest] < heap[left]:
+                largest = left
+            if right <= len(heap)-1 and heap[largest] < heap[right]:
+                largest = right
+            if largest != idx:
+                heap[idx], heap[largest] = heap[largest], heap[idx]
+                rearrangement_maxHeap(largest)
+        
+        # max_heap에서 최댓값 추출
         def extraction():
-            extracted = maxHeap[1]
-            maxHeap[1] = maxHeap[len(maxHeap)-1]
-            maxHeap.pop()
-            extraction_maxHeap(1)
-            return extracted
+            extraced = heap[1]
+            heap[1] = heap[len(heap)-1]
+            heap.pop()
+            rearrangement_maxHeap(1)
+            return extraced
         
-                
         # 예외처리
         if not nums or not k:
-            return 0
-        
+            return 0 
+
         # initialize
-        maxHeap = [None]
-        
+        heap = [None]
+        result = ''
+
         # Array -> maxHeap
-        for _ in range(len(nums)):
-            insert_maxheap(nums.pop())
+        for n in nums:
+            insert_maxHeap(n)
         
-        # k번째 추출
-        result=''
+        # 최대값(root) 추출 * k
         for _ in range(k):
             result = extraction()
-            
+        
         return result
             
-        
-        
-        
