8522e9b9cfb22b9f0b1c055c14a2e11a2d994b25
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-09-17 16:43:13 +0900

    Time: 265 ms (59.76%), Space: 27.9 MB (80.60%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -1,32 +1,29 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.endOfWord = False
-        
 class Trie:
     def __init__(self):
-        self.root = TrieNode()
-        
+        self.trie = {}
+
     def insert(self, word: str) -> None:
-        cur = self.root                        
-        for c in word:                        
-            if c not in cur.children:         
-                cur.children[c] = TrieNode() 
-            cur = cur.children[c]             
-        cur.endOfWord = True          
+        table = self.trie
+        for char in word:
+            if char not in table:
+                table[char] = {}
+            table = table[char]
+        table['@'] = {}
+        
+        
 
     def search(self, word: str) -> bool:
-        cur = self.root
-        for c in word:
-            if c not in cur.children:
-                return False         
-            cur = cur.children[c]  
-        return cur.endOfWord
-    
-    def startsWith(self, prefix: str) -> bool:
-        cur = self.root
-        for c in prefix:
-            if c not in cur.children:
+        table = self.trie
+        for char in word:
+            table = table.get(char, None)
+            if table is None:
                 return False
-            cur = cur.children[c]
-        return True 
+        return ( '@' in table )
+        
+    def startsWith(self, prefix: str) -> bool:
+        table = self.trie
+        for char in prefix:
+            table = table.get(char, None)
+            if table is None:
+                return False
+        return True
