f3ef511d4a0718d9c2af0216b7df8e2f6a5f2ebd
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-09-23 14:39:20 +0900

    Time: 556 ms (56.98%), Space: 16.7 MB (50.25%) - LeetHub

--- 147-insertion-sort-list/147-insertion-sort-list.py
+++ 147-insertion-sort-list/147-insertion-sort-list.py
@@ -5,13 +5,13 @@
 #         self.next = next
 class Solution:
     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return
-        cur = parent = ListNode(None)
+        cur = parent = ListNode(0)
         while head:
             while cur.next and cur.next.val < head.val:
                 cur = cur.next
+                
             cur.next, head.next, head = head, cur.next, head.next
             
-            cur = parent
-        return cur.next
+            if head and cur.val > head.val:
+                cur = parent
+        return parent.next
