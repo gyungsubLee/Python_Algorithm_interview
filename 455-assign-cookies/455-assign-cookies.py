3bde816a5a41f82f2ecb9a063fa40b564e1fe15d
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-09-30 18:10:15 +0900

    Time: 396 ms (18.82%), Space: 15.8 MB (47.54%) - LeetHub

--- 455-assign-cookies/455-assign-cookies.py
+++ 455-assign-cookies/455-assign-cookies.py
@@ -1,15 +1,16 @@
+import bisect
 from typing import List
+
 
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         g.sort()
         s.sort()
 
-        child_i = cookie_j = 0
-        # 만족하지 못할 때까지 그리디 진행
-        while child_i < len(g) and cookie_j < len(s):
-            if s[cookie_j] >= g[child_i]:
-                child_i += 1
-            cookie_j += 1
-
-        return child_i
+        result = 0
+        for i in s:
+            # 이진 검색으로 더 큰 인덱스 탐색
+            index = bisect.bisect_right(g, i)
+            if index > result:
+                result += 1
+        return result
