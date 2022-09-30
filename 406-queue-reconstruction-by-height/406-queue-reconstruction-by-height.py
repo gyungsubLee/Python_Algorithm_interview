4bf92eac361ba24eca9ea56c9cb6a5f281c4eeae
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-09-30 16:29:14 +0900

    Time: 227 ms (35.57%), Space: 14.5 MB (63.80%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -1,11 +1,7 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        heap = []
-        for person in people:
-            heapq.heappush(heap, (-person[0], person[1]))
-        
-        result = []
-        while heap:
-            person = heapq.heappop(heap)
-            result.insert(person[1], [-person[0], person[1]])
-        return result
+        output=[] 
+        people.sort(key=lambda x: (-x[0], x[1]))                
+        for a in people:
+            output.insert(a[1], a)
+        return output  
