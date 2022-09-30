19e434f7a302c646db3a9003b4c48b0961b7a4fc
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-09-30 17:38:33 +0900

    Time: 2123 ms (5.06%), Space: 19.1 MB (74.15%) - LeetHub

--- 134-gas-station/134-gas-station.py
+++ 134-gas-station/134-gas-station.py
@@ -2,16 +2,12 @@ from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        # 모든 주유소 방문 가능 여부 판별
-        if sum(gas) < sum(cost):
-            return -1
-
-        start, fuel = 0, 0
-        for i in range(len(gas)):
-            # 출발점이 안되는 지점 판별
-            if gas[i] + fuel < cost[i]:
+        trip_tank, curr_tank, start, n = 0, 0, 0, len(gas)
+        for i in range(n):
+            trip_tank += gas[i] - cost[i]
+            curr_tank += gas[i] - cost[i]
+            if curr_tank < 0:
                 start = i + 1
-                fuel = 0
-            else:
-                fuel += gas[i] - cost[i]
-        return start
+                curr_tank = 0 
+        return start if trip_tank >= 0 else -1
+
