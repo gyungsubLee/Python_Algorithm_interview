b8bff6413dd30479e83b1a21ecc198fd5a43096e
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-10-04 14:07:18 +0900

    Time: 166 ms (97.47%), Space: 15.5 MB (85.50%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,10 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         dic = collections.defaultdict(int)
+        mid = len(nums) // 2
         for n in nums:
             dic[n] += 1
-        
-        mid = len(nums) // 2
-        for n in dic:
             if dic[n] > mid:
                 return n
