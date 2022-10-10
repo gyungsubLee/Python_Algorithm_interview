d5ddae9438ca83be814af80b983b02cab0b6b8c0
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-10-10 15:44:46 +0900

    Time: 64 ms (25.48%), Space: 13.8 MB (97.21%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,17 +1,17 @@
-import collections
-from typing import List
-
-
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums:
+        length = len(nums)
+        if length == 0:
             return 0
-        if len(nums) <= 2:
+        if length == 1:
+            return nums[0]
+        if length == 2:
             return max(nums)
-
-        dp = collections.OrderedDict()
+        
+        dp = [0] * length
         dp[0], dp[1] = nums[0], max(nums[0], nums[1])
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
-
-        return dp.popitem()[1]
+        
+        for i in range(2, length):
+            dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+        
+        return max(dp)
