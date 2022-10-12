35db70a0dc5a0fdb127c792cd78789d27d33fbf9
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-10-12 22:32:51 +0900

    Time: 171 ms (77.12%), Space: 16 MB (81.96%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -7,13 +7,12 @@ class Solution:
             while stack and height[i] > height[stack[-1]]:
                 top = stack.pop()
                 
-                if not len(stack):
+                if not stack:
                     break
                 
-                distance = i - stack[-1] - 1
+                distance = i - stack[-1] -1
                 waters = min(height[i], height[stack[-1]]) - height[top]
-                
-                volume += distance * waters               
-            
+                volume += distance * waters
             stack.append(i)
+        
         return volume
