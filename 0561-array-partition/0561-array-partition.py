ff2888899ccf982161f6d53b9c381faa12f55b02
이경섭 <95308384+gyungsubLee@users.noreply.github.com>
2022-10-16 16:31:46 +0900

    Time: 555 ms (49.60%), Space: 16.8 MB (56.26%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -1,20 +1,4 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        nums.sort()
-        res, odd, even =0, 0, 1
-        lenght = len(nums)
-        
-        # 짝수
-        if lenght % 2 == 0:
-            while odd < lenght:
-                res += nums[odd]
-                odd += 2
-            return res
-        
-        # 홀수
-        if lenght % 2 == 1:
-            while even < lenght:
-                res += nums[even]
-                even += 2
-            return res
+        return sum(sorted(nums)[::2])
             
